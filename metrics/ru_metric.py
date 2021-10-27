942c7aa50bff312d622259073a8383f16fde07c2
daryna <dardem96@gmail.com>
2021-10-27 11:47:52 +0300

    STA metric: classifier update

--- metrics/ru_metric.py
+++ metrics/ru_metric.py
@@ -1,7 +1,7 @@
 import argparse
-import pandas as pd
+import numpy as np
 
-from style_trasnfer_accuracy import get_style_transfer_acc
+from style_trasnfer_accuracy import classify_preds
 from content_similarity import get_cosine_sim_corpus, calc_bleu, get_word_overlap_corpus
 from language_quality import get_gpt2_ppl_corpus
 from aggregation_metric import get_gm
@@ -10,20 +10,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', "--inputs", help="path to test sentences", required=True)
     parser.add_argument('-p', "--preds", help="path to predictions of a model", required=True)
+    parser.add_argument("--batch_size", default=32, type=int)
     
     args = parser.parse_args()
     
     with open(args.inputs, 'r') as input_file, open(args.preds, 'r') as preds_file:
         original_sentences = input_file.readlines()
         transferred_sentences = preds_file.readlines()
-        
+
     file_name = args.preds.split('.')[0]
-    df = pd.DataFrame({'comment': transferred_sentences, 'toxic': 0})
-    df.to_csv(file_name+'.csv', index=False)
     
     results_file = open(file_name+'_results.txt', 'w')
-    
-    acc = get_style_transfer_acc(file_name+'.csv')
+
+    accuracy_by_sent = classify_preds(args, transferred_sentences)
+    acc = np.mean(accuracy_by_sent)
     results_file.write('Style transfer accuracy: ' + str(acc) + '\n')
     
     cs = get_cosine_sim_corpus(original_sentences, transferred_sentences)
